57a4c846e684dc1a700a11ba2c4197cb0886cbc5
Sean Scofield <sescofield@c02ww05ljg5j.myfamily.int>
2021-02-19 22:26:08 -0800

    slight refactoring to sqs polling logic

--- src/application.py
+++ src/application.py
@@ -9,14 +9,12 @@ sqs_queue_url = os.environ['SQS_QUEUE_URL']
 sqs_client = boto3.client('sqs', region_name='us-east-1', endpoint_url=endpoint_url)
 
 def main():
+    print(f"Continually polling SQS queue {sqs_queue_url}...", flush=True)
     while True:
-        print(f"Polling SQS queue {sqs_queue_url}...", flush=True)
-        response = sqs_client.receive_message(QueueUrl=sqs_queue_url)
-        if 'Messages' in response:
-            for message in response['Messages']:
-                print(f'Received message {message["MessageId"]}', flush=True)
-                sqs_client.delete_message(QueueUrl=sqs_queue_url, ReceiptHandle=message['ReceiptHandle'])
-                print(f'Successfully processed message {message["MessageId"]}', flush=True)
+        for message in sqs_client.receive_message(QueueUrl=sqs_queue_url).get('Messages', []):
+            print(f'Received message {message["MessageId"]}', flush=True)
+            sqs_client.delete_message(QueueUrl=sqs_queue_url, ReceiptHandle=message['ReceiptHandle'])
+            print(f'Successfully processed message {message["MessageId"]}', flush=True)
 
         print('Sleeping for 3 seconds...', flush=True)
         time.sleep(3)
